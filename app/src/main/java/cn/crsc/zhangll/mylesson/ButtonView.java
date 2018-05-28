9a4fbc75a11163ab674f931f5f9ceaf95dc952bd
walterbiship <295675424@qq.com>
2018-05-28 12:56:16 +0800

    增加另外课程的两个Activity和实现讲课内容

---
+++ app/src/main/java/cn/crsc/zhangll/mylesson/ButtonView.java
@@ -0,0 +1,17 @@
+package cn.crsc.zhangll.mylesson;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by zhangll on 2018/5/28.
+ */
+
+public class ButtonView extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.radio_layout);
+    }
+}
